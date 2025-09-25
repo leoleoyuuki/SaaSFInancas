fac5b9ada9bfbae0c495d74c5d20ebf9c3c530c2
Leonardo Yuuki Nakazone <leoyuuki2005@gmail.com>
2025-09-25 13:19:50 +0000

    Olha como está

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -3,7 +3,6 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import {
   Menu,
-  LayoutDashboard,
 } from "lucide-react";
 
 export default function Header() {
@@ -25,44 +24,49 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 z-50">
-      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-        <Link
-          href="/dashboard"
-          className="flex items-center gap-2 text-lg font-semibold md:text-base"
-        >
-          <Logo/>
-          <span className="sr-only">FinanceFlow</span>
-        </Link>
-        <h1 className="text-2xl font-semibold font-headline text-foreground">
-          FinanceFlow
-        </h1>
-      </nav>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button
-            variant="outline"
-            size="icon"
-            className="shrink-0 md:hidden"
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <Link
+            href="/"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base"
           >
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <nav className="grid gap-6 text-lg font-medium">
-            <Link
-              href="#"
-              className="flex items-center gap-2 text-lg font-semibold"
+            <Logo/>
+            <span className="sr-only">FinanceFlow</span>
+          </Link>
+          <h1 className="text-2xl font-semibold font-headline text-foreground">
+            FinanceFlow
+          </h1>
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
             >
-              <Logo />
-              <span className="sr-only">FinanceFlow</span>
-            </Link>
-            <Link href="/dashboard" className="hover:text-foreground">
-              Dashboard
-            </Link>
-          </nav>
-        </SheetContent>
-      </Sheet>
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <nav className="grid gap-6 text-lg font-medium">
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-lg font-semibold"
+              >
+                <Logo />
+                <span className="sr-only">FinanceFlow</span>
+              </Link>
+              <Link href="/dashboard" className="hover:text-foreground">
+                Dashboard
+              </Link>
+               <Link href="/" className="text-muted-foreground hover:text-foreground">
+                Home
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </div>
     </header>
   );
 }
