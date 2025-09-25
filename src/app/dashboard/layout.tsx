4e95922e34067f9baa658a2c38d7a1e078c704a7
Leonardo Yuuki Nakazone <leoyuuki2005@gmail.com>
2025-09-25 13:30:37 +0000

    Nao, o problema nao esta em cada secao, provavelmente na hora de renderi

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col bg-background">
       <Header />
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <main className="container flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         {children}
       </main>
     </div>
